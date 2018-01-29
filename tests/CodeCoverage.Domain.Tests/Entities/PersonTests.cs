6f51a1ed2e8885b907fb491b7a61adb7393ac03d
tiago.pariz <tiago.pariz@totvs.com.br>
2018-01-29 17:44:14 -0200

    Domain tests project

--- tests/CodeCoverage.Domain.Tests/Entities/PersonTests.cs
+++ tests/CodeCoverage.Domain.Tests/Entities/PersonTests.cs
@@ -10,14 +10,14 @@ namespace CodeCoverage.Domain.Tests.Entities
         [Test]
         public void VerifyName()
         {
-            var sut = new Person(Guid.NewGuid(), "Test name");
+            var sut = new Person(Guid.NewGuid(), "Test name", null, null, null, null);
             Assert.AreEqual("Test name", sut.Name);
         }
 
         [Test]
         public void VerifyId()
         {
-            var sut = new Person(Guid.NewGuid(), "Test name");
+            var sut = new Person(Guid.NewGuid(), "Test name", null, null, null, null);
             Assert.AreNotEqual(Guid.Empty, sut.Id);
         }
     }
