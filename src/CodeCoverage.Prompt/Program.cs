96cf39986673b260d852288eb2be5123a037709d
tiago.pariz <tiago.pariz@totvs.com.br>
2018-01-29 16:58:24 -0200

    Console project

--- src/CodeCoverage.Prompt/Program.cs
+++ src/CodeCoverage.Prompt/Program.cs
@@ -6,23 +6,31 @@ namespace CodeCoverage.Prompt
 {
     internal class Program
     {
-        private StateDto stateDto;
-        private CityDto cityDto;
-        private PersonDto personDto;
+        private static StateDto _stateDto;
+        private static CityDto _cityDto;
+        private static PersonDto _personDto;
 
         private static void Main()
         {
             var state = new State(Guid.NewGuid(), "RS");
             var city = new City(Guid.NewGuid(), "Porto Alegre", state.Id, state);
             var person = new Person(Guid.NewGuid(), "Tiago", city.Id, state.Id, city, state);
+            SetMappings(person, city, state);
+
+            Console.WriteLine($"Name: {_personDto.Name}");
+            Console.WriteLine($"City: {_personDto.CityDto.Name}");
+            Console.WriteLine($"State: {_personDto.StateDto.Name}");
+
+            Console.ReadKey();
         }
 
-        private void Mappings(Person person,
-                              City city,
-                              State state)
+        private static void SetMappings(Person person,
+                                        City city,
+                                        State state)
         {
-            stateDto = new StateDto(state.Id, state.Name);
-            cityDto = new CityDto(city.Id, city.Name, city.StateId, stateDto);
+            _stateDto = new StateDto(state.Id, state.Name);
+            _cityDto = new CityDto(city.Id, city.Name, city.StateId, _stateDto);
+            _personDto = new PersonDto(person.Id, person.Name, person.StateId, person.CityId, _cityDto, _stateDto);
         }
     }
 }
